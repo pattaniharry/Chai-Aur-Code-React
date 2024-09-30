c84de7732d1167a8694bb08bdb60f72828581bf4
harry pattani <harrypattani15@gmail.com>
2024-09-30 12:37:48 +0530

    mega project setup - env variable

---
+++ 11MegaBlog/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+console.log(import.meta.env.VITE_APPWRITE_URL)
+
+  return (
+    <>
+    <h1 className='text-blue-800'> hello harry</h1>
+    </>
+  )
+}
+
+export default App
